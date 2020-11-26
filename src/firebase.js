b7e18c0f8768455036d484a5ff1533a2cc15ea56
Bob <rellrocker235@gmail.com>
2020-11-26 22:19:26 +0200

    Adding things together
    Just Puting things in order

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpR5KaADvrsAaKiO7Jd4g909ZG0V7sg6o",
+    authDomain: "e-challenge2.firebaseapp.com",
+    databaseURL: "https://e-challenge2.firebaseio.com",
+    projectId: "e-challenge2",
+    storageBucket: "e-challenge2.appspot.com",
+    messagingSenderId: "642775862775",
+    appId: "1:642775862775:web:b793bdce2dd0b9c5cab2f2"
+  };
