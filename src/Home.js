bc65351dce1c162dfa1ec50c393e9db703971189
Bob <rellrocker235@gmail.com>
2020-11-30 21:06:53 +0200

    Removal of Uneccesary Parts and Addition of Main Body
    Main Body Constrcution Part 1

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_container">
+                <img className="home_image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/CyberMonday/Promo/Desktop/Fuji_TallHero_CM_ZA_en_US_1x._CB416100405_.png"
+                alt="" />
+
+                <div className="home_row">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
