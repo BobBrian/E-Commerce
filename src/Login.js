5edf89112b61c77bee5b2e167fb1ca184eda9d83
Bob <rellrocker235@gmail.com>
2020-12-11 18:48:14 +0200

    Login
    Addition of Css code to Make Login Page more stylized , as well as buttons to begin functions for back end authentication

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 
 function Login() {
+
+    // These Here were Made to Track the Emails and Passowords of Users
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    // This is here to prevent the page for refreshing. purely for aesthetics
+    const signIn = e =>{
+        e.preventDefault()
+    }
+
+    const register = e =>{
+        e.preventDefault()
+    }
+    
+
+
+
     return (
+
         
         <div className="login">
             <Link to ='/'>
@@ -15,20 +33,25 @@ function Login() {
             <div className="login_container">
                 <h1>Sign In</h1>
                 <form>
+                    {/* basically when a user types in an email this function will automatically set the email value to what the user typed in
+                    mapping them to the const variable , same Princeple applies to the password */}
                     <h5>E-mail</h5>
-                    <input type='text'/>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value) }        />
 
                     <h5>Password</h5>
-                    <input type='password'/>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button>Sign In</button>
+
+
+                    {/* When the Signin Button is pressed it will intiate a function labelled signIn*/} 
+                    <button type = 'submit' onClick ={signIn} className="login_signinbutton">Sign In</button>
                 </form>
                 <p>
                     This mutual confidentiality agreement sample is for two company owners planning to merge their businesses.
                     They wish for important company inside information to remain confidential throughout their business dealings,
                     so they sign an agreement that obligates them to refrain from disclosing private matters to others.
                 </p>
-                <button>Create Amazon Account</button>
+                <button className="login_registerbutton" onClick = {register}>Create Amazon Account</button>
 
             </div>
             
